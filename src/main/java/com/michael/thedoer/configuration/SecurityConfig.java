f1c759f14b362ec2ba13dd2de8f4552e38c1456a
Michael Tolulope <olagunjum99@gmail.com>
2025-11-20 14:16:09 +0100

    authorization jwt without roles completed

--- src/main/java/com/michael/thedoer/configuration/SecurityConfig.java
+++ src/main/java/com/michael/thedoer/configuration/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         "/thedoer/v1/auth/**",
                                         "/swagger-ui/**",
+                                        "/swagger-ui.html",
                                         "/v3/api-docs"
                                 ).permitAll()
                                 .anyRequest().authenticated()
